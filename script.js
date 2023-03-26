60d41413755315d0963baa5939d8a16bfe94b93b
Jair Torezone <jairtorezone@gmail.com>
2023-03-26 19:49:54 -0300

    developed the generatePassword function logic

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Ola mundo!");
+function generatePassword() {
+  const chars =
+    "abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789?!@&*()[]";
+
+  let password = "";
+
+  for (let i = 0; i < 16; i++) {
+    const randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber + 1);
+  }
+
+  console.log(password);
+}
+
+generatePassword();
